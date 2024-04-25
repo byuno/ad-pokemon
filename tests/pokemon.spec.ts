e7554e70938a3d21e2cc039e498cc41b7463ce33
byuno <robert.y.uno@gmail.com>
2024-04-25 10:11:38 -0700

    waitForResponse attempt

--- tests/pokemon.spec.ts
+++ tests/pokemon.spec.ts
@@ -1,12 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach( async({ page }) => {
-
-    await page.goto('https://pokeapi.co/api/v2/pokemon');
-
-})
+// test.beforeEach( async({ page }) => {
+//     // await page.route('*/**/', async route => {
+//     //     const response = await route.fetch();
+//     //     const responseBody = await response.json();
+//     //     console.log(responseBody.results);
+//     // })
+//     await page.goto('https://pokeapi.co/api/v2/pokemon');
+// })
 
 test('test 1', async ({ page }) => {
-    await page.waitForTimeout(1000);
+    await page.goto('https://pokeapi.co/api/v2/pokemon');
+    const pokemonResponse = await page.waitForResponse('https://pokeapi.co/api/v2/pokemon');
+    const pokemonResposeBody = await pokemonResponse.json()
+    console.log(pokemonResposeBody)
+    //await page.waitForTimeout(1000);
 })
 
