2ae6fadf7d25884e1bbc15f54439c2100492b20e
byuno <robert.y.uno@gmail.com>
2024-05-02 19:10:36 -0700

    code clean up

--- tests/pokemon.spec.ts
+++ tests/pokemon.spec.ts
@@ -1,23 +1,6 @@
 import { test, expect, request, APIRequestContext , chromium} from '@playwright/test';
 
-// test.beforeEach( async({ page }) => {
-//     // await page.route('*/**/', async route => {
-//     //     const response = await route.fetch();
-//     //     const responseBody = await response.json();
-//     //     console.log(responseBody.results);
-//     // })
-//     await page.goto('https://pokeapi.co/api/v2/pokemon');
-// })
-
-test('test 1', async ({ page }) => {
-    const pokemonResponse = await page.waitForResponse('https://pokeapi.co/api/v2/pokemon');
-    await page.goto('https://pokeapi.co/api/v2/pokemon');
-    const pokemonResposeBody = await pokemonResponse.json()
-    console.log(pokemonResposeBody)
-    //await page.waitForTimeout(1000);
-})
-
-test.only('APIRequestContext attempt', async () => {
+test('APIRequestContext attempt', async () => {
     const APIRequestContext = await request.newContext();
     const response = await APIRequestContext.get('https://pokeapi.co/api/v2/pokemon');
     const responseBody = await response.json();
